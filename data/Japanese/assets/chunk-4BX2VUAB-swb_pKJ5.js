7c688a5114d027d735c5025c5fd602b9d3b4a331
ボムソク\beoms <beomseoklee90@gmail.com>
2026-06-26 13:33:03 +0900

    Updates

---
+++ data/Japanese/assets/chunk-4BX2VUAB-swb_pKJ5.js
@@ -0,0 +1,1 @@
+import{_ as i}from"./index-D9REO1rb.js";function t(c,e){c.accDescr&&e.setAccDescription?.(c.accDescr),c.accTitle&&e.setAccTitle?.(c.accTitle),c.title&&e.setDiagramTitle?.(c.title)}i(t,"populateCommonDb");export{t as p};
